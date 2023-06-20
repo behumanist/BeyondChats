89ee1dd33d6d37b183046d826a00b3f19357d721
Vijay Prakash Mishra <vijaymishra.07dec@gmail.com>
2023-06-20 14:02:28 +0530

    Update No 3
    - added sticky
    -changed static messages to dynamics using state lists

--- public/js/index.js
+++ public/js/index.js
@@ -3,20 +3,41 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
   data() {
     return {
-      tab: 'profile',
-      subtab: 'chat',
       cur_msg: "",
-      active: {profile_tab: "images", },
-      msg: { val: "This is Jaiprakash Mishra", ts: 1685970103000 },
+      active: {
+        profile_tab: "images",
+        tab: 'chats',
+        subtab: 'chat',
+      },
+      msgs: [
+        { type: 'msg', val: "This is Jaiprakash Mishra", ts: 1685970103000, by: '' },
+        { type: 'msg', val: "How are you", latitude: '', ts: 1685970103000, by: '' },
+        { type: 'maps', latitude: 28.6162944, longitude: 77.053952, ts: 1685970103000,  by: '' },
+        { type: 'poll', 
+          options: [{val: "Option 1", by: []},
+          {val: "Option 2", by: ["vijay", "hulk"]},
+          {val: "Option 3", by: ["saurav"]},
+          {val: "Option 4", by: ["Mishra"]},], ts: 1685970103000,  by: '' },
+
+        { type: 'sticky', ts: 1685970103000},
+        { type: 'maps', latitude: 28.6162944, longitude: 77.053952, ts: 1685970103000,  by: '' },
+        { type: 'maps', latitude: 28.6162944, longitude: 77.053952, ts: 1685970103000,  by: '' },
+        { type: 'maps', latitude: 28.6162944, longitude: 77.053952, ts: 1685970103000,  by: '' },
+
+        { type: 'maps', latitude: 28.6162944, longitude: 77.053952, ts: 1685970103000,  by: '' },
+        { type: 'maps', latitude: 28.6162944, longitude: 77.053952, ts: 1685970103000,  by: '' },
+
+      ],
       dimensions: { width: window.innerWidth, height: window.innerHeight },
-      stamp: 'WednesDay',
       months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      audioChunks: [],
     }
 
   },
+  computed: {
+  },
   mounted() {
+
     this.$nextTick(() => {
       window.addEventListener('resize', this.onResize);
       window.addEventListener('load', this.setId);
@@ -26,6 +47,10 @@ createApp({
     window.removeEventListener('resize', this.onResize);
   },
   methods: {
+    sortedArray(list) {
+      let x = [...list]
+      return x.sort((a,b)=>b.by.length-a.by.length);
+    },
     onResize() {
       this.dimensions.width = window.innerWidth;
       this.dimensions.height = window.innerHeight;
@@ -42,29 +67,29 @@ createApp({
 
     },
     activeTab(x) {
-      if (x == this.tab) { return true }
+      if (x == this.active.tab) { return true }
       return false;
     },
 
     startRecord() {
       navigator.mediaDevices.getUserMedia({ audio: true }).
-          then(stream => {
-              audioChunks = [];
-              rec = new MediaRecorder(stream);
-              rec.ondataavailable = e => {
-                  audioChunks.push(e.data);
-                  if (rec.state == "inactive") {
-                      let blob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
-                      recordedAudio.src = URL.createObjectURL(blob);
-                      recordedAudio.controls = true;
-                      sendData(blob);
-                  }
-              };
-              rec.start();
-              sendBtn.onmouseup = e => { rec.stop() };
-          });
+        then(stream => {
+          audioChunks = [];
+          rec = new MediaRecorder(stream);
+          rec.ondataavailable = e => {
+            audioChunks.push(e.data);
+            if (rec.state == "inactive") {
+              let blob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
+              recordedAudio.src = URL.createObjectURL(blob);
+              recordedAudio.controls = true;
+              sendData(blob);
+            }
+          };
+          rec.start();
+          sendBtn.onmouseup = e => { rec.stop() };
+        });
 
-  }
-    
+    }
+
   }
 }).mount('#app')
